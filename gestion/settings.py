94f5a9a7bec5bdd993749e6c3f19e0bc573dfc28
smarin1987 <73258968+smarin1987@users.noreply.github.com>
2025-04-08 18:56:07 -0500

    Update settings.py

--- gestion/settings.py
+++ gestion/settings.py
@@ -45,14 +45,6 @@ TEMPLATES = [
     },
 ]
 
-# Base de datos (SQLite por defecto)
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
 # Configuración para archivos estáticos
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
